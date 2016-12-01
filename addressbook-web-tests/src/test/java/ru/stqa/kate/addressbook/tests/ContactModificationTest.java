a46288a71b5cbe3cec8b9e2f04bea5ebb6c5de16
Ekaterina Perminova <katepermin@gmail.com>
2016-11-30 22:18:20 -0800

    Тест падает на сравнении контактов

--- addressbook-web-tests/src/test/java/ru/stqa/kate/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/kate/addressbook/tests/ContactModificationTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.stqa.kate.addressbook.appmanager.ContactHelper;
 import ru.stqa.kate.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -14,22 +15,27 @@ import java.util.List;
 public class ContactModificationTest extends TestBase{
   @Test
 
-  public void testContactModification(){
+  public void testContactModification() {
 
     app.getNavigationHelper().gotoHomePage();
-    if (!app.getContactHelper().isThereAContact()){
+    if (!app.getContactHelper().isThereAContact()) {
       app.getNavigationHelper().gotoAddNewPage();
       app.getContactHelper().createContact(new ContactData("Adam", "Smith", "Ady", "Apple", "+16190000000", "+16191111111", "adam@gmail.com", "Kate"), true);
     }
-   // app.getNavigationHelper().gotoHomePage();
+    // app.getNavigationHelper().gotoHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Contact12","Test","T", "YYY","UUU","789","567", "hh"), false);
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Contact12", "Test", "T", "YYY", "UUU", "789", "567", "hh");
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().updateContactModification();
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size());
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
   }
--- addressbook-web-tests/src/test/java/ru/stqa/kate/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/kate/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.kate.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -22,10 +23,16 @@ public class GroupModificationTests extends TestBase{
     List<GroupData> before = app.getGroupsHelper().getGroupList();
     app.getGroupsHelper().selectGroup(before.size()-1);
     app.getGroupsHelper().initGroupModification();
-    app.getGroupsHelper().fillGroupForm(new GroupData("Kate", null, "Fish"));
+    GroupData group = new GroupData(before.get(before.size()-1).getId(),"Kate", null, "Fish");
+
+    app.getGroupsHelper().fillGroupForm(group);
     app.getGroupsHelper().submitGroupModification();
     app.getGroupsHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
